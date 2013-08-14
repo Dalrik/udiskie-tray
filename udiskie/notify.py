50bfde182c90f6cbee91d9b947918ee99658d3ca
Ian Thompson <dalrik370@gmail.com>
2013-08-13 20:13:25 -0400

    Upgrade notification system to use libnotify

--- udiskie/notify.py
+++ udiskie/notify.py
@@ -1,22 +1,22 @@
-import pynotify
-import gio
+from gi.repository import Notify as GNotify
+from gi.repository import Gio
 
 class Notify:
     def __init__(self, name):
-        pynotify.init(name)
+        GNotify.init(name)
 
     def mount(self, device, path):
         try:
-            pynotify.Notification('Device mounted',
+            GNotify.Notification.new('Device mounted',
                                   '%s mounted on %s' % (device, path),
                                   'drive-removable-media').show()
-        except gio.Error:
+        except Gio.Error:
             pass
 
     def umount(self, device):
         try:
-            pynotify.Notification('Device unmounted',
+            GNotify.Notification.new('Device unmounted',
                                   '%s unmounted' % (device,),
                                   'drive-removable-media').show()
-        except gio.Error:
+        except Gio.Error:
             pass
