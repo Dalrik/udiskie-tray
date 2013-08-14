dcc6deef6670d20ce480d3bf7945f89f0a267f64
Ian Thompson <dalrik370@gmail.com>
2013-08-14 19:29:02 -0400

    Rebrand as udiskie-tray and reset version number

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from distutils.core import setup
 
 setup(
-    name='udiskie',
-    version='0.4.2',
+    name='udiskie-tray',
+    version='0.1.0',
     description='Removable disk automounter for udisks',
-    author='Byron Clark',
-    author_email='byron@theclarkfamily.name',
-    url='http://bitbucket.org/byronclark/udiskie',
+    author='Ian Thompson',
+    author_email='dalrik370@gmail.com',
+    url='http://github.com/Dalrik/udiskie-tray',
     license='MIT',
     packages=[
         'udiskie',
